c037cb88d7b53219c6fa839108a1e02a19935647
Aaron299792 <aaron.sanabria@ucr.ac.cr>
2024-06-29 01:05:07 -0600

    the configuration_update was improved and implemented only for H= 0

--- ISING_CPP/spin.cpp
+++ ISING_CPP/spin.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <random>
 #include <cmath>
 #include <omp.h>
@@ -25,7 +26,7 @@ Spin &Spin::operator=(const Spin &obj){
   N = obj.N;
   lattice = obj.lattice;
   energy = obj.energy;
-
+  
   return *this;
 }
 
@@ -46,8 +47,9 @@ int Spin::get_magnetization(){
   int magnetization = 0;
   #pragma omp parallel reduction (+ : magnetization)
   {
-    #pragma omp for
+    #pragma omp for schedule(static)
     for (unsigned int i = 0; i < N*N; ++i){
+      #pragma omp atomic
       magnetization += lattice[i];
     }
   }
@@ -72,30 +74,37 @@ void Spin::flip(const unsigned int &row, const unsigned int &col){
   lattice[ (row*N) + col ] = (lattice[ (row*N) + col ] > 0) ? -1 : 1;
 }
 
-Spin Spin::configuration_update(const double &beta, const float &J, const float &H, const unsigned int &max_iter){
+Spin Spin::configuration_update(const double &beta, const float &J, const unsigned int &max_iter){
   int dE = 0;
-  double dH = 0.0;
-
-  std::random_device rd; 
-  std::mt19937 rng(rd());
-  std::uniform_int_distribution<unsigned int> rndint(0, N);
-  std::uniform_real_distribution<double> rndb(0, 1);
   
-  #pragma omp parallel reduction(+ : energy) private(dE, dH, rng, rndint, rndb)
+  std::random_device rd;
+  std::mt19937 rng(rd());
+  //std::uniform_int_distribution<unsigned int> rndint(0, N - 1);
+  std::uniform_real_distribution<double> rndb(0, 1);
+
+  std::unordered_map<int, double> probability = {
+    {-8, exp(beta * -8*J)},
+    {-4, exp(beta * -4*J)},
+    {0, exp(beta * 0*J)},
+    {4, exp(beta * 4*J)},
+    {8, exp(beta * 8*J)},
+  };
+
+  #pragma omp parallel reduction(+ : energy) private(dE, dH)
   {
-    #pragma omp for
+    #pragma omp for schedule(static)
     for (unsigned int i = 0; i < max_iter; ++i){
-      unsigned int row = rndint(rng)%N;
-      unsigned int col = rndint(rng)%N;
+			unsigned int row = static_cast<unsigned int>( floor(rndb(rng) * N));
+      unsigned int col = static_cast<unsigned int>( floor(rndb(rng) * N));
       Spin lattice_copy = *this;   
       lattice_copy.flip(row,col);
 
       dE = lattice_copy.close_neighbord_energy(row, col);
-      dH = -H*(lattice_copy.get_magnetization() - get_magnetization()) - J*dE;
-      if (rndb(rng) < exp(-beta*dH)){
-        #pragma omp atomic
-        energy += dE;
+      //dH = -H*(lattice_copy.get_magnetization() - get_magnetization()) - J*dE;
+      if (rndb(rng) < probability[dE]){
         *this = lattice_copy;
+        #pragma omp atomic 
+        energy += dE;
       }
     }
   }
