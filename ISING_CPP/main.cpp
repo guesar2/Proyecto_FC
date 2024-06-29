39a2a96d09091266c207cd077085c14184056fa0
Aaron299792 <aaron.sanabria@ucr.ac.cr>
2024-06-29 01:04:10 -0600

    changes in main.cpp (Just playing with paramenters)

--- ISING_CPP/main.cpp
+++ ISING_CPP/main.cpp
@@ -5,9 +5,9 @@
 #include "spin.hpp"
 
 int main(){
-  unsigned int N = 10;
+  unsigned int N = 50;
   Spin lattice(N);
-  lattice.configuration_update(1.48, 1.0, 0.0, 10000).print_lattice();
+  lattice.configuration_update(0.00, 1.0, 10000).print_lattice();
   std::cout << "Energy: " << lattice.get_energy() << " " <<
                "Magnetization: " << lattice.get_magnetization() << std::endl;
 
